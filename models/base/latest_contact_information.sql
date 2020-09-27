82ef3a6acb7b758b4c15c229441c41576c5f431a
Henri Dumon <henridumon93@outlook.com>
2020-09-27 15:26:14 +0000

    Data source update

--- models/base/latest_contact_information.sql
+++ models/base/latest_contact_information.sql
@@ -5,5 +5,5 @@ select all_contact_data.vid contact_id
       ,ARRAY_AGG(concat(properties.firstname.value, " ",properties.lastname.value) ORDER BY _sdc_extracted_at DESC)[OFFSET(0)] AS contact_full_name
       ,ARRAY_AGG(properties.country.value ORDER BY _sdc_extracted_at DESC)[OFFSET(0)] AS contact_country
       ,ARRAY_AGG(properties.state.value ORDER BY _sdc_extracted_at DESC)[OFFSET(0)] AS contact_state
-FROM `freelance-290006.hubspot.contacts` all_contact_data
+FROM `{{target.project}}.hubspot.contacts` all_contact_data
 group by all_contact_data.vid
